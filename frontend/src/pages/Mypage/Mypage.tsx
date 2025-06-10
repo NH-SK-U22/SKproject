e6b5665c8de2c106ab466379285b6a69bfa8a64d
shuto <yamamotshuto0505@gmail.com>
2025-06-10 10:54:00 +0900

    マージ用push

--- frontend/src/pages/Mypage/Mypage.tsx
+++ frontend/src/pages/Mypage/Mypage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styles from './mypage.module.css'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import Card from '../../components/Mypage/Card'
 
 const Mypage = () => {
   const [tabs,setTabs]=useState(0)
@@ -31,32 +32,22 @@ const Mypage = () => {
         </div>
         {tabs===0?(
           <div id="cards" className={`${styles.tab_content} ${tabs === 0 ? ` ${styles.active}` : ''}`}>
+            
             <div className={styles.bottom}>
-              <div className={styles.card}>
-                <div className={styles.card_image}></div>
-                <div className={styles.card_text}>使用する</div>
-              </div>
-              <div className={styles.card}>
-                <div className={styles.card_image}></div>
-                <div className={styles.card_text}>使用する</div>
-              </div>
-              <div className={styles.card}>
-                <div className={styles.card_image}></div>
-                <div className={styles.card_text}>使用する</div>
-              </div>
-              <div className={styles.card}>
-                <div className={styles.card_image}></div>
-                <div className={styles.card_text}>使用する</div>
-              </div>
+              <Card text="それ" point={6} click="/mypage"/>
+              <Card text="それ" point={6} click="/mypage"/>
+              <Card text="それ" point={6} click="/mypage"/>
+              <Card text="それ" point={6} click="/mypage"/>
             </div>
           </div>):""}
         
         {tabs===1?(
           <div id="history" className={`${styles.tab_content} ${tabs === 1 ? ` ${styles.active}` : ''}`}>
             <ul className={styles.history_list}>
-              <li className={styles.history_item}>ユーザーA: 開発進捗はどうですか？</li>
-              <li className={styles.history_item}>ユーザーB: 昨日のバグ修正が完了しました。</li>
-              <li className={styles.history_item}>ユーザーC: 次のミーティングは明日10時です。</li>
+              <li className={styles.history_item}>チョコたっぷり: 美味しいと思います</li>
+              <li className={styles.history_item}>FIFA: ボール持ったフランスのが持ってない日本より速いです</li>
+              <li className={styles.history_item}>きのこ派: 手が汚れにくいですよね???</li>
+              <li className={styles.history_item}>タスク:ランクの表示画像、残りのポイントプログレスバー</li>
             </ul>
           </div>):""}
 
